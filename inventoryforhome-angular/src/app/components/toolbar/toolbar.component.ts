f95f58684afe1f1fd890996065cc8173105a4ea1
Yazbeth <yazryrg@outlook.com>
2025-04-09 00:48:56 -0600

    +Cambio del sidenav por un menu en el toolbar

--- inventoryforhome-angular/src/app/components/toolbar/toolbar.component.ts
+++ inventoryforhome-angular/src/app/components/toolbar/toolbar.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+// import {MatSidenavModule} from '@angular/material/sidenav';
+// import {MatListModule} from '@angular/material/list';
 import { RouterModule } from '@angular/router'; // ✅ IMPORTANTE
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, RouterModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
